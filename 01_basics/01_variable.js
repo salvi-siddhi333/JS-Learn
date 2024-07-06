80f72cc501c945508277fa513cad1fde8abb21d8
Siddhi Salvi <siddhi.salvi@sakec.ac.in>
2024-07-06 05:14:30 +0000

    modifications

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+var userName = "Admin"
+let userState = "Mumbai"
+const userPass = "Testpass@1234"
+const userID = 9631;
+
+console.table([userPass, userName, userState, userID])
---
+++ 01_basics/02_datatypes&ECMA.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Samarth")
+
+
+let name = "Samarth"
+let age = 108
+let isLoggedIn = false
+let state;
+let value = null;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
