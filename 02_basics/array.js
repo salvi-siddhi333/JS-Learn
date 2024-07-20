cbff9de94510a187aa72cbfd131bf79c6b63b297
Siddhi Salvi <siddhi.salvi@sakec.ac.in>
2024-07-20 09:58:18 +0000

    added more on arrays

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -27,17 +27,46 @@ MyNewArr.shift() //.unshift("value") will remove value at the starting position
 const myArrJoin = MyNewArr.join()
 
 // console.log(myArrJoin);
-console.log(MyNewArr);
+// console.log(MyNewArr);
 
 // console.log(typeof(myArrJoin));
 // console.log(typeof(MyNewArr));
 
 //SLICE , SPLICE
 
-console.log("A ", MyNewArr);
+// console.log("A ", MyNewArr);
 const a = MyNewArr.slice(1,4)
-console.log(a);
+// console.log(a);
 
-console.log("B ", MyNewArr)
+// console.log("B ", MyNewArr)
 const b = MyNewArr.splice(1,4)
-console.log(b);
+// console.log(b);
+
+// ---------------------
+
+fruits = ["Apple", "Orange", "Mango"]
+mtypes = ["Alphanso", "Totapuri", "Bitki"]
+// console.log(fruits);
+// fruits.push(mtypes)
+// console.log(fruits[3][1]);
+
+// const newfruits = fruits.concat(mtypes)
+// console.log(newfruits);
+
+const newfruits = [...fruits, ...mtypes] //spread 
+// console.log(newfruits);
+
+const newAnotherArray = [1,2,3,4,[5,6,7],8,[9,10,[11,12,13],[14,15]]]
+// console.log(newAnotherArray.flat(Infinity))
+
+
+//data scraping
+// console.log(Array.isArray("Siddhi"))
+// console.log(Array.from("Siddhi"))
+// console.log(Array.from({name:"Siddhi"})) //interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
