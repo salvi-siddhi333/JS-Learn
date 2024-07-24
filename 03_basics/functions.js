8aa0fbda9cf4a18ffc2b731c3f10bc89be435f63
Siddhi Salvi <siddhi.salvi@sakec.ac.in>
2024-07-24 20:38:54 +0000

    sunctions started

---
+++ 03_basics/functions.js
@@ -0,0 +1,12 @@
+function add(num1, num2){
+    console.log(num1+num2);
+}
+
+add(3,2)
+
+// -------------------------STRUCTURE
+// function function_name(format){
+//     function_defination
+// }
+
+// function_reference ()//()-function_execution
